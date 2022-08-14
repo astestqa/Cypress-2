602f6960e549a010e83606d60fa76f075c96c26a
Alberto <mnsialer@gmail.com>
2022-08-14 11:57:41 -0400

    Premier version projet

---
+++ cypress/e2e/Bonjour_monde.cy.js
@@ -0,0 +1,20 @@
+
+
+describe('Bienvenu formation de Cypress section 1', () => {
+
+    it('Test-1 -> Bonjour monde', () => {
+        cy.log("Bonjour Monde");
+        cy.wait(2000);
+        
+    });
+
+    it('Test-2 -­> champ name', () => {
+        cy.visit("https://demoqa.com/text-box");
+        Cypress.on('uncaught:exception', (err) => {
+            return false
+        })
+        cy.get("#userName").type("Alberto");
+        cy.wait(2000);
+    });
+
+})
