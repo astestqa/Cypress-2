6c470e3b7fc9963a76f30c1a24be2787a66934a9
Alberto <mnsialer@gmail.com>
2022-08-30 21:44:32 -0400

    version 0.16

--- ci/e2e.sh
+++ ci/e2e.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd cypressgit/
+cd cypressgit
 
 #npx cypress run 
 #$(npm bin)/cypress run
 
 ls
 
-#npm run cy:ci
+npm run cy:ci
 
-npm run testHeadless
+#npm run testHeadless
 
 #npm run withHeadTest
